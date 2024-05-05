ea64dc77f4512773ef7cd8d1ec9b6fd0549f0400
Everesh <everesh@proton.me>
2024-05-05 16:55:39 +0200

    Normalize argument case

--- substrings.rb
+++ substrings.rb
@@ -1,7 +1,7 @@
 def substrings(master, dictionary)
 
     dictionary.reduce(Hash.new(0)) do | accumulator, word |
-        accumulator[word] += master.scan(word).length if master.include?(word)
+        accumulator[word] += master.downcase.scan(word).length if master.downcase.include?(word)
         accumulator
     end
 
